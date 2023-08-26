9ab34091a03abd7bcd0eab489a39288c12b478e9
gzero1016 <gzero1016@gmail.com>
2023-08-26 21:21:40 +0900

    feat: TitleButton 클릭시 페이지이동

--- src/component/HeaderLayout/Title/Title.jsx
+++ src/component/HeaderLayout/Title/Title.jsx
@@ -2,15 +2,22 @@ import React from 'react';
 /** @jsxImportSource @emotion/react */
 import * as S from "./Style";
 import TitleButton from './TitleButton/TitleButton';
+import { Link } from 'react-router-dom';
 
 function Title(props) {
     return (
         <div css={S.SLayout}>
             <div css={S.SHeaderLayout}>
                 <div css={S.SHeader}>
-                    <TitleButton name="피드" />
-                    <TitleButton name="타임라인" />
-                    <TitleButton name="리뷰" />
+                    <Link to={"/feed"}>
+                        <TitleButton name="피드" />
+                    </Link>
+                    <Link to={"/timeline"}>
+                        <TitleButton name="타임라인" />
+                    </Link>
+                    <Link to={"/review"}>
+                        <TitleButton name="리뷰" />
+                    </Link>
                     <TitleButton name="예약주문" />
                     <TitleButton name="저장" />
                 </div>
